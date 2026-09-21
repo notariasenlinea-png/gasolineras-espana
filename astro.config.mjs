eab5fa44467ac136faa667b121f250ebd559320d
Aitor Soriano <aisoriano02@gmail.com>
2026-09-21 22:38:24 +0200

    fix: update canonical domain to gasolinerasenlinea.es, add ads.txt and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://gasolineras-espana.es',
+  site: 'https://gasolinerasenlinea.es',
   integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
 });
